3c6b43f365d0b3ec61e124abbeac1747e4fee016
NF RIDOY <nfridoy@gmail.com>
2023-05-19 20:21:11 +0600

    Input Index and Details btn done

--- Javascript/json.js
+++ Javascript/json.js
@@ -1,9 +1,10 @@
 document.getElementById("showUsers").addEventListener("click", function () {
     //  alert("Clicked");
     document.getElementById("User-List").style.display = "block";
-
-    loadData();
-})
+    const userCount = document.getElementById("user-count").value;
+    console.log(userCount);
+    loadData(userCount);
+});
 
 // async function loadData(URL) {
 //     const responce = await fetch("URL");
@@ -21,26 +22,54 @@ document.getElementById("showUsers").addEventListener("click", function () {
 // }
 
 // ------------
-function loadData() {
+function loadData(userCount) {
     const usersList = document.getElementById("User-List");
+    usersList.innerHTML = "";
     fetch("https://jsonplaceholder.typicode.com/users")
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                const names = data[i].name;
-                console.log(names);
-                const userNames = document.createElement("li");
-                userNames.innerHTML = names;
-                usersList.appendChild(userNames);
+        .then((res) => res.json())
+        .then((data) => {
+            if (userCount == "") {
+                // userCount = data.length;
+            } else {
+                for (let i = 0; i < data.slice(0,userCount) ; i++) {
+                    const names = data[i].name;
+                    console.log(names);
+                    const userNames = document.createElement("li");
+                    
+                    userNames.innerHTML = `Name: ${names}. <button ="id${data[i].id}" onclick="showDetails(${data[i].id})">show Details ${data[i].id}</button>`;
+                    // userNames.setAttribute.setAttribute("class","userNamesli")
+                    usersList.appendChild(userNames);
+                }
             }
-
-        })
+        });
 }
-document.getElementById("hideUsers").addEventListener("click", function(){
+document.getElementById("hideUsers").addEventListener("click", function () {
     deleteData("User-List");
-})
+});
 
 function deleteData(id) {
     console.log("Hide");
-    document.getElementById(id).innerHTML=" ";
+    document.getElementById(id).innerHTML = " ";
+}
+function showDetails(details, userNames) {
+    console.log("Show Details Clicked");
+    fetch(`https://jsonplaceholder.typicode.com/users/${details}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.id);
+            // const li = document.getElementById(`id${details}`);
+            // const li = document.getElementsByClassName("userNamesli");
+            const ulUserDetails = document.getElementById("user-details");
+            const liUser = document.createElement("li");
+            liUser.innerHTML = `
+            <p>Id: ${data.id}</p>
+            <p>name: ${data.name}</p>
+            <p>username: ${data.username}</p>
+            <p>email: ${data.email}</p>
+            <p>Address: ${data.address}</p>
+            <p>Id: ${data.id}</p>
+            <p>Id: ${data.id}</p>`;
+            ulUserDetails.appendChild(liUser);
+        });
 }
